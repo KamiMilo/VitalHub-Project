f1421d2310da5306c8f808f39e47836c0a2eeab0
kamille <kamille.senai@gmail.com>
2024-02-22 10:51:17 -0300

    configuraçoes do modal na home

--- senai-vitalHub-reactNativeStyledComponentsExpo/src/screens/Home/Home.js
+++ senai-vitalHub-reactNativeStyledComponentsExpo/src/screens/Home/Home.js
@@ -9,30 +9,37 @@ import { ContainerButtons } from "./style"
 import { useState } from "react"
 import { ListComponent } from "../../components/List/List"
 import { AbsListAppointment } from "../../components/AbsListAppointment/AbsListAppointment"
-import { Card } from "../../components/Card/Card"
+import CancellationModal from "../../components/CancellationModal/CancellationModal"
+import { AppointmentCard } from "../../components/AppointmentCard/AppointmentCard"
+import ProntuarioModal from "../../components/ProntuarioModal/ProntuarioModal"
 
 
 const Consultas = [
-    {id: 1, nome: "Allan" ,situacao:"pendente"},
-    {id: 2, nome: "Evelyn Oliveira" ,situacao:"realizado"},
-    {id: 3, nome: "Carlos" ,situacao:"cancelado"},
-    {id: 4, nome: "Sara" ,situacao:"pendente"}
+    { id: 1, nome: "Allan", situacao: "pendente" },
+    { id: 2, nome: "Evelyn Oliveira", situacao: "realizado" },
+    { id: 3, nome: "Carlos", situacao: "cancelado" },
+    { id: 4, nome: "Sara", situacao: "pendente" }
 ]
 
 export const Home = () => {
 
     //Hook para renderizar a lista dependendo de qual botão for clicado
-    const [statusLista,setStatusLista] = useState("pendente")
-    return(
+    const [statusLista, setStatusLista] = useState("pendente")
+    // Satate para os modais
+    const [showModalCancel, setShowModalCancel] = useState(false);
+    const [showModalAppointment, setShowAppointment] = useState(false);
+    
+    
+    return (
         <Container>
-            
-            <StatusBar/>
+
+            <StatusBar />
 
             {/* Header */}
-            <Header/>
+            <Header />
 
             {/* Calendar */}
-            <CalendarHome/>
+            <CalendarHome />
 
             {/* filterAppointment (Container dos botoes) */}
             {/* container */}
@@ -40,43 +47,56 @@ export const Home = () => {
 
             {/* Container  */}
             <ContainerButtons>
-                
-                <AbsListAppointment 
-                textButton={"Agendadas"}
-                clickButton={statusLista === "Agendadas"}
-                onPress={() => setStatusLista("pendente")}
-                />
-                
+
                 <AbsListAppointment
-                textButton={"Realizadas"}
-                clickButton={statusLista === "Realizadas"}
-                onPress={() => setStatusLista("realizado")}
+                    textButton={"Agendadas"}
+                    clickButton={statusLista === "Agendadas"}
+                    onPress={() => setStatusLista("pendente")}
                 />
 
-               
                 <AbsListAppointment
-                textButton={"Canceladas"}
-                clickButton={statusLista === "Canceladas"}
-                onPress={() => setStatusLista("cancelado")}
-                />               
+                    textButton={"Realizadas"}
+                    clickButton={statusLista === "Realizadas"}
+                    onPress={() => setStatusLista("realizado")}
+                />
+
+
+                <AbsListAppointment
+                    textButton={"Canceladas"}
+                    clickButton={statusLista === "Canceladas"}
+                    onPress={() => setStatusLista("cancelado")}
+                />
             </ContainerButtons>
 
 
             {/* Cards */}
             {/* Lista */}
             <ListComponent
-            data= {Consultas}
-            keyExtractor={(item) => item.id}
+                data={Consultas}
+                keyExtractor={(item) => item.id}
 
-            renderItem={({item}) => statusLista == item.situacao && (
-                <Card
-                situacao={item.situacao} 
-                />
-            )
-        }
-            
-            
+                renderItem={({ item }) => statusLista == item.situacao && (
+                    <AppointmentCard
+                        situacao={item.situacao}
+                        onPressCancel={() => setShowModalCancel(true)}
+                        onPressAppointment={() => setShowAppointment(true)}
+                    />
+                )
+                }
+
             />
+            
+                <CancellationModal
+                    visible={showModalCancel}
+                    setShowModalCancel={setShowModalCancel}
+                />
+
+                <ProntuarioModal
+                visible={showModalAppointment}
+                setShowAppointment={setShowAppointment}
+                />
+
+
 
         </Container>
 
