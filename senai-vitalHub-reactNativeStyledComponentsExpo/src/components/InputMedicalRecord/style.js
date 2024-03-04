0adcf4d49dc3b6714e182cf13091070f57559edb
kamille <kamille.milo@aluno.senai.br>
2024-03-04 13:03:36 -0300

    tela de prontuario finalizado

---
+++ senai-vitalHub-reactNativeStyledComponentsExpo/src/components/InputMedicalRecord/style.js
@@ -0,0 +1,15 @@
+import { styled } from "styled-components/native";
+
+export const Input = styled.TextInput.attrs({
+    placeholderTextColor: "#49B3BA"
+  })`
+    justify-content: flex-start;
+    width: 90%;
+    height:117px;
+    padding: 16px;
+    border-radius: 5px;
+    border: 2px solid #49B3BA;
+    margin-top: 15px;
+    font-size: 15px;
+    font-family: 'MontserratAlternates_600SemiBold';
+  `
--- senai-vitalHub-reactNativeStyledComponentsExpo/src/screens/medicalRecord/medicalRecord.js
+++ senai-vitalHub-reactNativeStyledComponentsExpo/src/screens/medicalRecord/medicalRecord.js
@@ -1,52 +1,69 @@
-import { Button, ButtonGoogle, ButtonTitle } from "../../components/Button/style"
+import { ScrollView } from "react-native"
+import { Button, ButtonGoogle, ButtonSecondary, ButtonSecondaryTitle, ButtonTitle } from "../../components/Button/style"
 import { ModalText } from "../../components/CancellationModal/style"
-import { Container } from "../../components/Container/style"
+
 import { Input } from "../../components/Input/style"
 import { ModalProfileName } from "../../components/ProntuarioModal/style"
 import { ContainerProntuario, ContainetDados, ImageProntuario, InputProntuario, SubtitleMr, TextData } from "./style"
-
-export const MedicalRecord = ({navigation}) => {
-return(
-   
-    <>
-    <ImageProntuario source={{uri:"https://github.com/evy-oliveira0807.png" }} />
-    
-    <ModalProfileName>Evelyn Oliveira</ModalProfileName>  
-
-    {/* // idade e email: text */}
-
-    <ContainetDados>
-    <TextData>19 Anos</TextData>
-    <TextData>Evy_oliveira@gmail.com</TextData>
-    </ContainetDados>
-
-   
-    <ContainerProntuario>
-    
-    {/* // subtitulo */}
-
-    <SubtitleMr>Descrição da consulta</SubtitleMr>
-    <InputProntuario placeholder="Descrição da consulta" />
-    
-    <SubtitleMr>Diagnóstico do paciente</SubtitleMr>
-    <Input placeholder="Diagnóstico do paciente" />
-    
-    <SubtitleMr>Prescrição médica</SubtitleMr>
-    <InputProntuario placeholder="Prescrição médica " />
+import { LinkBold } from "../../components/LinkMedium/styles"
+import { Container } from "../../components/Container/style"
+// import { InputMedicalRecord } from "../../components/InputMedicalRecord/InputMedicalRecord"
 
 
-   
-</ContainerProntuario>
+export const MedicalRecord = ({ navigation }) => {
+    return (
+
+        <ScrollView>
+
+            <ImageProntuario source={{ uri: "https://github.com/evy-oliveira0807.png" }} />
+
+            <ModalProfileName>Evelyn Oliveira</ModalProfileName>
+
+            {/* // idade e email: text */}
+
+            <ContainetDados>
+                <TextData>19 Anos</TextData>
+                <TextData>Evy_oliveira@gmail.com</TextData>
+            </ContainetDados>
+
+
+            <ContainerProntuario>
+
+                {/* // subtitulo */}
+
+                <SubtitleMr>Descrição da consulta</SubtitleMr>
+                <InputProntuario placeholder="Descrição da consulta" />
+
+                <SubtitleMr>Diagnóstico do paciente</SubtitleMr>
+                <Input placeholder="Diagnóstico do paciente" />
+
+                <SubtitleMr>Prescrição médica</SubtitleMr>
+                <InputProntuario placeholder="Prescrição médica " />
 
 
 
-    <Button>
-        <ButtonTitle>Salvar</ButtonTitle>
-    </Button>
+            </ContainerProntuario>
 
-    
-    </>
-    
-)
+
+            <Container style={{ marginTop: 20 }}>
+
+                <Button>
+                    <ButtonTitle>Salvar</ButtonTitle>
+                </Button>
+            </Container>
+
+            <ButtonSecondary style={{justifyContent: "center"}}>
+
+            <ButtonSecondaryTitle style={{textAlign:"center", paddingLeft: 20}}> 
+                Cancelar
+            </ButtonSecondaryTitle>
+
+            </ButtonSecondary>
+
+
+
+        </ScrollView>
+
+    )
 
 }
